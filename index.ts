03e03beca422a053a9f85e61e7af41cbc930b9a0
Віталій <vitaliy19980422@gmail.com>
2026-05-17 14:32:42 +0300

    refactor: update script source and reorganize TypeScript imports

---
+++ index.ts
@@ -0,0 +1,8 @@
+import './src/task-01';
+import './src/task-02';
+import './src/task-03';
+import './src/task-04';
+import './src/task-05';
+import './src/task-06';
+import './src/task-07';
+import './src/task-08';
