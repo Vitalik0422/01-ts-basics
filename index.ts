a4e8f91d082422ee21e1c43e556aa5cf2cc6d25d
Віталій <vitaliy19980422@gmail.com>
2026-05-17 14:34:26 +0300

    feat: add task files with initial implementations and update imports

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import './src/task-01';
-import './src/task-02';
-import './src/task-03';
-import './src/task-04';
-import './src/task-05';
-import './src/task-06';
-import './src/task-07';
-import './src/task-08';
+import './src/task-1';
+import './src/task-2';
+import './src/task-3';
+import './src/task-4';
+import './src/task-5';
+import './src/task-6';
+import './src/task-7';
+import './src/task-8';
